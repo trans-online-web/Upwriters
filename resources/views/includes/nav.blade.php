24cefaf68bc2525f12260878f80d74c0fc2e3fbf
jeremihz <mihjere68@gmail.com>
2019-11-27 19:09:16 +0300

    dashboard link

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -44,7 +44,6 @@
                                         </a>
 
                                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                            <a href="/dashboard" class="dropdown-item">My Dashboard</a>
                                             <a class="dropdown-item" href="/myorder">My Orders</a>
                                             <a class="dropdown-item" href="{{ route('logout') }}"
                                                onclick="event.preventDefault();
