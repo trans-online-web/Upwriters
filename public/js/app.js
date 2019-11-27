7805fd34527f33d6f4af19a6aca1a0c6b3c12cb7
jeremihz <mihjere68@gmail.com>
2019-11-27 18:58:41 +0300

    dashboard link

--- public/js/app.js
+++ public/js/app.js
@@ -123573,8 +123573,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /opt/lampp/htdocs/Transonline/Up/Upwriters/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /opt/lampp/htdocs/Transonline/Up/Upwriters/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\mihz\Desktop\web\Upwriters\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\mihz\Desktop\web\Upwriters\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -44,6 +44,7 @@
                                         </a>
 
                                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                            <a href="/dashboard" class="dropdown-item">My Dashboard</a>
                                             <a class="dropdown-item" href="/myorder">My Orders</a>
                                             <a class="dropdown-item" href="{{ route('logout') }}"
                                                onclick="event.preventDefault();
