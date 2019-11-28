5a38cc91a5c8f143caab0987d33c80a24f3119d0
shemnduati <nduatishem@gmail.com>
2019-11-28 16:53:18 +0300

    paypal btn

--- resources/views/email/payment.blade.php
+++ resources/views/email/payment.blade.php
@@ -219,4 +219,18 @@
     </tr>
 </table>
 </body>
+<script>
+    paypal.Buttons({
+        createOrder: function(data, actions) {
+            // This function sets up the details of the transaction, including the amount and line item details.
+            return actions.order.create({
+                purchase_units: [{
+                    amount: {
+                        value: '0.01'
+                    }
+                }]
+            });
+        }
+    }).render('#paypal-button-container');
+</script>
 </html>
